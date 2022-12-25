65827444def8341d06af93f104d5b6766daa22d8
Jeff Kingyens <jeff.kingyens@gmail.com>
2022-12-25 12:16:52 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,160 @@
+import fs from 'fs';
+import { S3 } from "@aws-sdk/client-s3";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import handlebars from 'handlebars';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const s3Client = new S3({
+    forcePathStyle: false, 
+    endpoint: process.env.DO_SPACE_ENDPOINT,
+    region: "us-east-1",
+    credentials: {
+      accessKeyId: process.env.DO_SPACE_KEY,
+      secretAccessKey: process.env.DO_SPACE_SECRET
+    }
+});
+
+// parse the test file 
+
+let app = fs.readFileSync(__dirname + '/test/binder.json')
+let parsedApp = JSON.parse(app)
+let outputApp = { } 
+
+// copy over name, author and version
+outputApp.name = { }
+outputApp.name.bundle = parsedApp.name.bundle
+outputApp.name.display = parsedApp.name.display
+outputApp.author = parsedApp.author
+outputApp.version = parsedApp.version
+
+// generate output to 'out'
+if (!fs.existsSync(__dirname + '/out')){
+    fs.mkdirSync(__dirname + '/out');
+} else {
+    fs.rmSync(__dirname + '/out', { recursive: true})
+    fs.mkdirSync(__dirname + '/out');
+}
+
+// create a bucket for this class
+import { CreateBucketCommand } from "@aws-sdk/client-s3";
+const bucketParams = { Bucket: parsedApp.name.bundle };
+
+const run = async () => {
+    try {
+      const data = await s3Client.send(new CreateBucketCommand(bucketParams));
+      // console.log("Success", data.Location);
+      return data;
+    } catch (err) {
+      // console.log("Error", err);
+    }
+  };
+  
+let data = run().then(function (d){ 
+    console.log(d)
+})
+
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+
+// iterate over the keys of contents
+outputApp.contents = { }
+let keys = Object.keys(parsedApp.contents)
+for (let i = 0; i < keys.length; i++) { 
+    
+    let v = keys[i]
+    let name = v;
+    let type = parsedApp.contents[v].type
+    let source = parsedApp.contents[v].source
+
+    // lets upload this video to our digital ocean account
+    const bucketParams = {
+        Bucket: parsedApp.name.bundle,
+        Key: source,
+        Body: fs.readFileSync(__dirname + '/test/' + source),
+        ACL: 'public-read',
+        ContentType: type
+    };
+
+    let run = async () => {
+        try {
+        const data = await s3Client.send(new PutObjectCommand(bucketParams));
+        console.log(
+            "Successfully uploaded object: " +
+            bucketParams.Bucket +
+            "/" +
+            bucketParams.Key
+        );
+        return data;
+        } catch (err) {
+        console.log("Error", err);
+        }
+    };    
+    run();
+
+    // don't add type here because its already stored with the bucket
+    outputApp.contents[v] = { 
+        source: process.env.DO_SPACE_ENDPOINT + '/' + parsedApp.name.bundle + '/' + source
+    }
+
+}
+
+// generate a package file that can be directly hosted on digital ocean storage blob
+
+fs.writeFileSync(__dirname + '/out/binder.json', JSON.stringify(outputApp, ' ', 4), 'utf8')
+
+// output the link to the course contents (which in term references deployed resources)
+async function run2() {
+    try {
+    const data = await s3Client.send(new PutObjectCommand({
+        Bucket: parsedApp.name.bundle,
+        Key: 'binder.json',
+        Body: JSON.stringify(outputApp, ' ', 4),
+        ACL: 'public-read',
+        ContentType: 'application/json'
+    }));
+    return data;
+    } catch (err) {
+    console.log("Error", err);
+    }
+};
+run2()
+
+console.log(process.env.DO_SPACE_ENDPOINT + '/' + parsedApp.name.bundle + '/binder.json')
+
+let contents = [ ];
+Object.keys(outputApp.contents).forEach( function (v, i) { 
+    contents.push({ 
+        url: outputApp.contents[v].source
+    })
+})
+
+// render app template using handlebars?
+let templ = handlebars.compile(fs.readFileSync(__dirname + '/template.html', 'utf8'));
+let htmlOutput = templ({ 
+    self_url: process.env.DO_SPACE_ENDPOINT + '/' + parsedApp.name.bundle + '/binder.html',
+    display: parsedApp.name.display, 
+    contents: contents
+})
+
+// console.log(htmlOutput)
+
+// lets build an html page for these video files as well
+// output the link to the course contents (which in term references deployed resources)
+async function run3() {
+    try {
+    const data = await s3Client.send(new PutObjectCommand({
+        Bucket: parsedApp.name.bundle,
+        Key: 'binder.html',
+        Body: htmlOutput,
+        ACL: 'public-read',
+        ContentType: 'text/html'
+    }));
+    return data;
+    } catch (err) {
+    console.log("Error", err);
+    }
+};
+run3()
+
+console.log(process.env.DO_SPACE_ENDPOINT + '/' + parsedApp.name.bundle + '/binder.html')
---
+++ readme.md
@@ -0,0 +1,53 @@
+# Binder
+
+Binder is a compiler that takes online course cirriculum (videos, audio and pdf) and deploys it into a user experience that is designed for actual users taking the course. Chances are, if you are taking an online class over the internet, it is being served by a learning management system (LMS) that was designed for the teacher it was being sold to. We provide a tool that empowers the user to get the experience they want. Learning new things is hard enough. The user should not have to fight the software or the platforms that access the content to make progress. 
+
+# Who is it for?
+
+Binder is made for teachers who want to make their own course material more accessible on mobile devices. It is also possible for students taking classes to package course material for a class they are taking and share it with other students taking the class. In this case, we recommend working with your teacher to make things official though. The goal is that if you have access to the course material and $5/mo to create a digital ocean space, you should be able to package the material in a simple way.
+
+# Setup
+
+1. Create or sign into your account on Digital Ocean
+
+2. Create a new space from the Digital Ocean developer console.
+
+3. Copy the endpoint URL and save it to a file called .env in this repo folder. Example:
+
+DO_SPACE_ENDPOINT=https://rh-pnt.nyc3.digitaloceanspaces.com
+
+4. Create a key/secret pair for this space and add it to the .env file created above. Example:
+
+DO_SPACE_KEY=DO00CE96V9TKWYEXHA6W
+DO_SPACE_SECRET=uAJYxWaawOPN6J6pQkavNhuYd7QQh/+VmOjI/NWv+HQ
+
+# Defining a Course
+
+Defining a course is easy. It's just a JSON file that tells binder the general navigation hierarchy of your content and gives it basic meta data, like a friendly name, the type of file it is, etc. Example:
+
+{ 
+    "name": "The Positive Neuroplasticity Training",
+    "author": "Rick Hanson",
+    "version": "0.1.0",
+    "contents": { 
+        "Class 1": { 
+            "type": "mp4",
+            "path": "PNT Class 1, Part 1.mp4"
+        }
+    }
+}
+
+# Deploying the Course
+
+Once you have defined the course, you will want to pass it through the compiler. Before deploying to digital ocean the compiler will validate the package for errors and issues that could arise. if the package passes validation than the program will be uploaded and you will receive a link that can be used to view the course on the web:
+
+https://rh-pnt.nyc3.digitaloceanspaces.com/main/0.1.0
+
+The underlying web format is based on Google's AMP project. We chose this because it is a simple and mobile friendly format based on HTML, making it compatible with web browsers today. 
+
+# Native Clients
+
+If you prefer, you can download a native client for Binder on either iOS or Android. Simply paste or drop the web link into the mobile client and it will be added to the list of courses in your binder. From there you can navigate and consume course material in the same way you can on the web. Over time, we expect the native clients to be more feature rich than the web, simply do to the availability of platform APIs. The links are here:
+
+iOS native client (source, app store link)
+Android native client (source, app store link)
