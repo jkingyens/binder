e4db573c8d4de50e0dada154eab79cfa94d2134c
Jeff Kingyens <jeff.kingyens@gmail.com>
2023-01-09 18:43:37 -0800

    parse userland json into java objects

--- android/app/src/main/java/com/substrait/MainActivity.java
+++ android/app/src/main/java/com/substrait/MainActivity.java
@@ -3,6 +3,7 @@ package com.substrait;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
@@ -18,8 +19,19 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.substrait.databinding.ActivityMainBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -56,7 +68,8 @@ private ActivityMainBinding binding;
                 AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
                 alert.setTitle(R.string.addurl_prompt);
                 EditText input = new EditText(getApplicationContext());
-
+                input.setTextColor(getResources().getColor(R.color.white));
+                input.setHint(R.string.addurl_hint);
 
                 input.setOnFocusChangeListener(new View.OnFocusChangeListener() {
                     @Override
@@ -80,6 +93,7 @@ private ActivityMainBinding binding;
                         Editable value = input.getText();
                         String out = value.toString();
                         Log.d("dfsdf", out);
+                        new JsonTask().execute(out);
                     }
                 });
                 alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -95,4 +109,156 @@ private ActivityMainBinding binding;
 
     }
 
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        protected String doInBackground(String... params) {
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                String jsonString = buffer.toString();
+                Log.d("sadfasdf", jsonString);
+
+                return jsonString;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Log.d("asdfsdf", result);
+
+
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(result);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Log.d("sadfasdf", jsonObject.toString());
+
+            // convert the JSON object into a real java object
+            ULApp theApp = new ULApp();
+
+            try {
+
+                theApp.name.bundle = (jsonObject.getJSONObject("name")).getString("bundle");
+                theApp.name.display = (jsonObject.getJSONObject("name")).getString("display");
+                theApp.author = jsonObject.getString("author");
+                theApp.version = jsonObject.getString("version");
+
+                JSONArray modules = jsonObject.getJSONArray("modules");
+                theApp.modules = new ULModule[modules.length()];
+
+                for (int i = 0; i < modules.length(); i++) {
+
+                    theApp.modules[i] = new ULModule();
+
+                    theApp.modules[i].name = modules.getJSONObject(i).getString("name");
+
+                    JSONArray videos = modules.getJSONObject(i).getJSONArray("videos");
+                    theApp.modules[i].videos = new ULVideo[videos.length()];
+
+                    for (int j = 0; j < videos.length(); j++) {
+
+                        theApp.modules[i].videos[j] = new ULVideo();
+
+                        theApp.modules[i].videos[j].name = videos.getJSONObject(j).getString("name");
+                        theApp.modules[i].videos[j].source = videos.getJSONObject(j).getString("source");
+
+                    }
+
+                }
+
+                Log.d("dsafsdf", theApp.toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    public class ULApp {
+
+        public ULApp() {
+
+            name = new ULAppName();
+
+        }
+
+        public ULAppName name;
+        public String author;
+        public String version;
+        public ULModule[ ] modules;
+
+        public class ULAppName {
+            public String bundle;
+            public String display;
+        }
+
+    }
+
+    public class ULModule {
+
+        public ULModule() {
+
+        }
+        public String name;
+        public ULVideo[ ] videos;
+
+    }
+
+    public class ULVideo {
+
+        public ULVideo() {
+
+        }
+
+        public String name;
+        public String source;
+    }
 }
